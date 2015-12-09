848bb36bab67ea83b45f96075713235746fd6c94
SirLagz <sirlagz@gmail.com>
2015-12-09 04:35:23 +0000

    updated core.py

--- core.py
+++ core.py
@@ -11,6 +11,7 @@ password = os.environ['SNPASSWORD']
 table = os.environ['SNTABLE']
 query = "updated"
 time = "Last6Months"
+database = "KDash"
 
 fields = ["assigned_to","assignment_group","category","contact_type","description","number","opened_at","opened_by","priority","short_description","state","sys_class_name","sys_created_on","sys_id","sys_updated_on","u_req_priority","u_requestor","u_affected_contact","u_business_service","u_resolution_code","u_resolution_reason","u_sla_progress"]
 
@@ -20,7 +21,7 @@ data = mod_getsndata.get_sn_table_data(instance, username, password, table, quer
 records = mod_trimsndata.trim_sn_data(data,fields)
 
 for record in records:
-    mongoid = mod_loadsndata.update_mongo_collection(args.database,args.table,records[record])
+    mongoid = mod_loadsndata.update_mongo_collection(database,table,records[record])
     print("Upserted "+str(mongoid))
 
 
@@ -28,5 +29,5 @@ while true:
     time.sleep(1800)
     data = mod_getsndata.get_sn_table_data(instance, username, password, table)
     records = mod_trimsndata.trim_sn_data(data,fields)
-    mongoid = mod_loadsndata.update_mongo_collection(args.database,args.table,records[record])
+    mongoid = mod_loadsndata.update_mongo_collection(database,table,records[record])
     print("Upserted "+str(mongoid))
