4d71202c628dc5c202170a2be452e65d36c0f772
SirLagz <sirlagz@gmail.com>
2015-12-09 03:56:18 +0000

    fixed typo

--- core.py
+++ core.py
@@ -12,7 +12,8 @@ table = os.environ['SNTABLE']
 query = "updated"
 time = "Last6Months"
 
-fields = ["assigned_to","assignment_group","category","contact_type","description","number","opened_at","opened_by","priority","short_description","state","sys_class_name","sys_created_on","sys_id","sys_upd$
+fields = ["assigned_to","assignment_group","category","contact_type","description","number","opened_at","opened_by","priority","short_description","state","sys_class_name","sys_created_on","sys_id","sys_updated_on","u_req_priority","u_requestor","u_affected_contact","u_business_service","u_resolution_code","u_resolution_reason","u_sla_progress"]
+
 
 data = mod_getsndata.get_sn_table_data(instance, username, password, table, query, time)
 
